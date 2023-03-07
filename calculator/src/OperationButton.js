b5d7ae2be6c6bcc6113cef1e2f430f4164cad562
Saurav Kumar Maurya <saurav.maurya@gmail.com>
2023-03-07 18:51:42 +0530

    revmoved car saurav

--- calculator/src/OperationButton.js
+++ calculator/src/OperationButton.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { ACTIONS } from './App'
 
 function OperationButton({ operation, dispatch }) {
-    var saurav;
     return (
         <>
             <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERTAION, payload: operation })}>{operation}</button>
