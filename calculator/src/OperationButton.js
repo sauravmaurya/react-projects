514f65d31f97ad0f791e7f65b00e69899328cde6
Saurav Kumar Maurya <saurav.maurya@gmail.com>
2023-03-07 18:49:46 +0530

    made changes to the calculator file

--- calculator/src/OperationButton.js
+++ calculator/src/OperationButton.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { ACTIONS } from './App'
 
 function OperationButton({ operation, dispatch }) {
+    var saurav;
     return (
         <>
             <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERTAION, payload: operation })}>{operation}</button>
