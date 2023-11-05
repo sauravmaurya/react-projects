93242ee7c2dc991388e6a56741b2114368984deb
Saurav Kumar Maurya <saurav.maurya@gmail.com>
2023-11-05 14:29:53 +0530

    added copy1 branch changes

--- git-test/test.js
+++ git-test/test.js
@@ -3,7 +3,7 @@ let embedobj = {
     b: { c: 1, d: 2 },
     e: { f: { g: 3, k: 9 } },
     h: 4,
-    i:{j:7}
+    i: { j: 7 }
 }
 
 const flattenObj = function (obj) {
@@ -25,3 +25,17 @@ const flattenObj = function (obj) {
 }
 
 console.log(flattenObj(embedobj))
+
+// #copy1 branch code
+
+const sum = function (a, b, ...args) {
+    console.log(a + b)
+    console.log(args)
+    let sum = 0
+    for (let x of args) {
+        sum += x
+    }
+    return sum
+}
+
+// console.log(sum(1,2,3,1,2,3))
